2a14abe9e4744c94ed81bd7c6c55ce33bf829a3d
Moc Meo <mocmeodx@hotmail.com>
2016-07-31 21:14:55 +0700

    [Enhancement] Set system default encoding as utf-8

--- single_webpage_crawler.py
+++ single_webpage_crawler.py
@@ -1,13 +1,16 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
 from urlparse import urljoin
 
-url = 'https://thenewboston.com/'
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+url = 'https://www.youtube.com/'
 content = requests.get(url)
-
-
 soup = BeautifulSoup(content.text, 'html.parser')
 
 for elem in soup.find_all("a", href=True):
     print elem.string
-    print urljoin(url, elem['href']) 
+    print urljoin(url, elem['href'])
