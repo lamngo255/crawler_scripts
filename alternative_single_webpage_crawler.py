f1daf414af16c2fe5989b8434c89e6e89fc8a5a0
Moc Meo <mocmeodx@hotmail.com>
2016-07-31 21:18:34 +0700

    Add alternative way for crawling single webpage

---
+++ alternative_single_webpage_crawler.py
@@ -0,0 +1,12 @@
+# find all links
+import requests
+import re
+
+url = raw_input("Enter a URL: ")
+
+website = requests.get(url)
+content = website.text
+links = re.findall('"((http|ftp)s?://.*?)"', content)
+
+for link in links:
+    print link[0]
