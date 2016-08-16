c65ae63efc6cc15ddecb84edfd5004a41eace004
Lam Ngo <mocmeodx@hotmail.com>
2016-08-16 11:22:13 +0700

    Add project Spider - multithreading

---
+++ Spider/main.py
@@ -0,0 +1,41 @@
+import threading
+from queue import Queue
+from domain import get_domain_name
+from spider import Spider
+
+
+NUM_SPIDERS = 10
+HOMEPAGE = 'https://twitter.com/'
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+Spider(DOMAIN_NAME, HOMEPAGE)
+q = Queue()
+
+
+def work():
+    while True:
+        url = q.get()
+        Spider.crawl_page(threading.currentThread().name, url)
+        q.task_done()
+
+
+def create_spiders():
+    for x in range(NUM_SPIDERS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+def create_jobs():
+    for link in Spider.queue:
+        q.put(link)
+    q.join()
+    crawl()
+
+
+def crawl():
+    if len(Spider.queue) > 0:
+        print(str(len(Spider.queue)) + " links in the queue")
+        create_jobs()
+
+create_spiders()
+crawl()
